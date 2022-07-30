d816b49445aa4e8ce0cda08344d63581c287d820
Jamilur Rahman <r.jamilur96@gmail.com>
2022-07-31 05:47:03 +0600

    beautification done

--- src/components/FilterTabs/FilterTabs.js
+++ src/components/FilterTabs/FilterTabs.js
@@ -5,7 +5,7 @@ export default function FilterTabs() {
   const {setCategory} = useContext(NewsContext)
   return (
     <div className="buttons d-flex justify-content-center my-2 pb-5 m-4">
-      <button className="btn btn-outline-dark me-2 rounded" onClick={()=>setCategory("business")}>#business</button>
+      <button className="btn btn-outline-dark me-2 rounded active-link" onClick={()=>setCategory("business")}>#business</button>
       <button className="btn btn-outline-dark me-2 rounded" onClick={()=>setCategory("sports")}>#sports</button>
       <button className="btn btn-outline-dark me-2 rounded" onClick={()=>setCategory("general")}>#general</button>
       <button className="btn btn-outline-dark me-2 rounded" onClick={()=>setCategory("entertainment")}>#entertainment</button>
