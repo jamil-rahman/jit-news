40ba7c1d6d8f3e05a3864b049e8776150aff2a59
Jamilur Rahman <r.jamilur96@gmail.com>
2022-07-31 05:22:53 +0600

    typo in index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { NewsProvider } from './context/NewsContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <NewsProvider> */}
     <App />
-    {/* </NewsProvider> */}
   </React.StrictMode>
 );
